cdc6c9aecf38caf6ca09862211c6f635cba0ce29
Matthew Short <short.matthew.f@gmail.com>
2018-09-08 17:10:06 -0400

    rebuild

--- app.21bccfbf.js
+++ app.21bccfbf.js
@@ -427,7 +427,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51112' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53350' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
